e0ef7a4caae9e4be96a9fa5fbdebcb85700ba5a2
setiawangunadii <setiawan.gunadi@daksa.co.id>
2024-04-02 14:50:34 +0700

    Setup Notification Everyday On 11 AM

--- lib/blocs/splash/splash_bloc.dart
+++ lib/blocs/splash/splash_bloc.dart
@@ -21,11 +21,6 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
     try {
       emit(OnLoadingSplash());
       emit(OnSuccessSplash());
-      // await SharedPrefsStorage.setTimeAlarm(
-      //     timeAlarm: Handler().formatDate(now).toString());
-      // await SharedPrefsStorage.setStatusAlarm(statusAlarm: true);
-      // await SharedPrefsStorage.setStatusMens(statusMens: true);
-      // await SharedPrefsStorage.setTotalAlarm(totalAlarm: 0);
     } on SessionExpired catch (e) {
       emit(OnErrorSplash(e.message));
     } on Network catch (e) {
